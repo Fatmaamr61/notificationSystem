093aa1d5d69bb31931e1e9668a090b794157b904
fatmaamr61 <fatma.amr6197@gmail.com>
2024-07-30 02:05:51 +0300

    edit html template

--- src/utils/generateHTML.js
+++ src/utils/generateHTML.js
@@ -76,7 +76,7 @@ export const signUpTemp = (link) => `<!DOCTYPE html>
             width: 100% !important;
         }
 
-        /* iOS #DC8991 LINKS */
+        /* iOS SteelBlue LINKS */
         a[x-apple-data-detectors] {
             color: inherit !important;
             text-decoration: none !important;
@@ -101,18 +101,26 @@ export const signUpTemp = (link) => `<!DOCTYPE html>
     </style>
 </head>
 
-<body style="background-color: #DC8991; margin: 0 !important; padding: 0 !important;">
+<body style="background-color: #0000FF; margin: 0 !important; padding: 0 !important;">
 <!-- HIDDEN PREHEADER TEXT -->
 <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> We're thrilled to have you here! Get ready to dive into your new account. </div>
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <!-- LOGO -->
     <tr>
-<tr>
-   <td style="background-image: url(https://res.cloudinary.com/dc4zgmrmf/image/upload/v1715029369/DiamondSoul/background.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center;" align="center">
+        <td bgcolor="SteelBlue" align="center">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                <tr>
+                    <td align="center" valign="top" style="padding: 40px 10px 40px 10px;"> </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="SteelBlue" align="center" style="padding: 0px 10px 0px 10px;">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
                     <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                        <h1 style="font-size: 45px; font-weight: 400; margin: 2;">Activation Mail</h1> <img src="https://res.cloudinary.com/dc4zgmrmf/image/upload/v1715029372/DiamondSoul/logo.png" width="400" height="420" style="display: block; border: 0px;" />
+                        <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Activation Mail</h1> <img src="https://cdn.discordapp.com/attachments/880207110607081505/1141403183692927046/image.png" width="125" height="120" style="display: block; border: 0px;" />
                     </td>
                 </tr>
             </table>
@@ -133,8 +141,8 @@ export const signUpTemp = (link) => `<!DOCTYPE html>
                                 <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 30px 30px;">
                                     <table border="0" cellspacing="0" cellpadding="0">
                                         <tr>
-                                            <td align="center" style="border-radius: 3px;" bgcolor="#DC8991">                            
-                                                <a href="${link}" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff ; text-decoration: none; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #DC8991; display: inline-block;">Activate Account</a> 
+                                            <td align="center" style="border-radius: 3px;" bgcolor="SteelBlue">                            
+                                                <a href="${link}" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff ; text-decoration: none; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid SteelBlue; display: inline-block;">Activate Account</a> 
                                             </td>
                                         </tr>
                                     </table>
@@ -152,7 +160,8 @@ export const signUpTemp = (link) => `<!DOCTYPE html>
                     <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
                         <p style="margin: 0;">Follow us on:</p>
                         <div>
-                            <a style="padding-right:20px" href="https://www.instagram.com/diamond_soul2023?igsh=M2pmaGlua2RrMjY3"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" width="30"></a>
+                            <a style="padding-right:10px" href="https://instagram.com/suraj_vsk"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" width="25"></a>
+                            <a href="https://twitter.com/suraj_vsk"><img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" width="25"></a>
                         </div>
                     </td>
                 </tr>
@@ -170,24 +179,28 @@ export const passwordResetTemplate = (code) => `<!DOCTYPE html>
     <title>Password Reset</title>
     <!-- Your meta tags and styles here -->
 </head>
-<body style="background-color: #ffffff; margin: 0 !important; padding: 0 !important;">
+<body style="background-color: SteelBlue; margin: 0 !important; padding: 0 !important;">
 <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> Password reset instructions. </div>
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <!-- LOGO -->
     <tr>
-        <td bgcolor="#ffffff" align="center">
+        <td bgcolor="SteelBlue" align="center">
             <!-- Your logo table here -->
         </td>
     </tr>
     <tr>
-   <td style="background-image: url(https://res.cloudinary.com/dc4zgmrmf/image/upload/v1715029369/DiamondSoul/background.jpg); background-size: cover; background-repeat: repeat; background-position: center center;" align="center">
+        <td bgcolor="SteelBlue" align="center" style="padding: 0px 10px 0px 10px;">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
-                    <td bgcolor="#ffffff" align="center" valign="top" style="padding: 30px 0px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                        <h1 style="font-size: 45px; font-weight: 400; margin: 5;">Password Reset</h1> <img src= "https://res.cloudinary.com/dc4zgmrmf/image/upload/v1715029372/DiamondSoul/logo.png" width="350" height="120" style="display: block; border: 0px;" />
+                    <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+                        <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Password Reset</h1> <img src="https://cdn.discordapp.com/attachments/880207110607081505/1142832746549477460/image.png" width="125" height="120" style="display: block; border: 0px;" />
                     </td>
                 </tr>
             </table>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
                     <td bgcolor="#ffffff" align="left">
@@ -195,21 +208,21 @@ export const passwordResetTemplate = (code) => `<!DOCTYPE html>
                             <tr>
                                 <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 30px 30px;">
                                     <table border="0" cellspacing="0" cellpadding="0">
+                                        <tr>
+                                            <td align="center" style="border-radius: 3px;" bgcolor="SteelBlue">                            
+                                                <a href="link" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff ; text-decoration: none; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid SteelBlue; display: inline-block;">Reset Password</a> 
+                                            </td>
+                                        </tr>
                                     </table>
                                 </td>
                             </tr>
                         </table>
-        </td>
-    </tr>
-    <tr>
-   <td style="background-image: url(https://res.cloudinary.com/dc4zgmrmf/image/upload/v1715029369/DiamondSoul/background.jpg); background-size: cover; background-repeat: repeat; background-position: center center;" align="center">
-            
                     </td>
                 </tr>
                 <tr>
                     <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 20px 30px; color: black; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
                         <p style="margin: 0;">To reset your password, please use the following code:</p>
-                        <p style="margin: 0; font-size: 24px; font-weight: bold; color: #DC8991;">${code}</p>
+                        <p style="margin: 0; font-size: 24px; font-weight: bold; color: SteelBlue;">${code}</p>
                     </td>
                 </tr>
                 <tr>
@@ -235,7 +248,6 @@ export const accountActivatedTemplate = () => `<!DOCTYPE html>
         body {
             font-family: Arial, sans-serif;
             text-align: center;
-            background-image: url('https://res.cloudinary.com/dc4zgmrmf/image/upload/v1715029369/DiamondSoul/background.jpg'); 
             background-size: cover;
             background-repeat: no-repeat;
             background-position: center;
@@ -253,10 +265,6 @@ export const accountActivatedTemplate = () => `<!DOCTYPE html>
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="logo">
-            <img src="https://res.cloudinary.com/dc4zgmrmf/image/upload/v1715029372/DiamondSoul/logo.png" alt="Your Logo" width="500"> <!-- Adjust width as needed -->
-        </div>
         <h1 width="200">Congratulations!</h1>
         <p>Your account has been activated. You can log in now.</p>
     </div>
